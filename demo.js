3869d0f00ae588cf8797c0b1e02d134851226c31
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2014-05-03 11:43:29 +0200

    Tweaks. Adds demo.js.

---
+++ demo.js
@@ -0,0 +1,18 @@
+function * gen() {
+  console.log(yield 1);
+  console.log(yield 2);
+  return 42;
+}
+
+// yield
+
+
+// .next()
+
+
+// .throws()
+
+
+// async
+
+
--- some-es6-code.js
+++ some-es6-code.js
@@ -1,3 +1,4 @@
+
 function async (generator) {
 
   var iter = generator(move);
